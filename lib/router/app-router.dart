213fa71ae513772b0a8424d78c7b4d07768a76f8
arun.kumar <arun.kumar@bcml.in>
2025-03-06 16:21:48 +0530

    fb

---
+++ lib/router/app-router.dart
@@ -0,0 +1,38 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter_auto_route/pages/dashboard.dart';
+import 'package:flutter_auto_route/pages/home.dart';
+
+@AutoRouterConfig(replaceInRouteName: 'Page,Route')
+class AppRouter extends RootStackRouter {
+  @override
+  List<AutoRoute> get routes => [
+    AutoRoute(path: '/', page: HomeRoute.page),
+    AutoRoute(path: '/dashboard', page: DashboardRoute.page),
+    RedirectRoute(path: '*', redirectTo: '/'),
+  ];
+}
+
+class HomeRoute extends PageRouteInfo<void> {
+  const HomeRoute({List<PageRouteInfo>? children})
+    : super(DashboardRoute.name, initialChildren: children);
+
+  static const String name = 'HomeRoute';
+
+  //  static const PageInfo<void> page = PageInfo<void>(name);
+  // static const PageInfo<StoryRouteArgs> page = PageInfo<S>(name);
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) => HomePage(title: 'Home'),
+  );
+}
+
+class DashboardRoute extends PageRouteInfo<void> {
+  const DashboardRoute({List<PageRouteInfo>? children})
+    : super(DashboardRoute.name, initialChildren: children);
+
+  static const String name = 'DashboardRoute';
+
+  //  static const PageInfo<void> page = PageInfo<void>(name);
+  // static const PageInfo<StoryRouteArgs> page = PageInfo<S>(name);
+  static PageInfo page = PageInfo(name, builder: (data) => DashboardPage());
+}
